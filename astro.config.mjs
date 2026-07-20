d9fbf0ee4cf9b5f325b9cc280771785d45541022
web708090 <adirassner@gmail.com>
2026-07-20 21:37:52 +0300

    Deploy preview to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from 'astro/config';
+
+const githubPages = process.env.SITE_TARGET === 'github-pages';
+
 export default defineConfig({
-  site: 'https://cyberdrtabansky.com',
+  site: githubPages ? 'https://adirass-web.github.io' : 'https://cyberdrtabansky.com',
+  base: githubPages ? '/LTPR' : undefined,
   i18n: { defaultLocale: 'en', locales: ['en', 'he'], routing: { prefixDefaultLocale: true } },
 });
